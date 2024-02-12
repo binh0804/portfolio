16e81530729aea1bc96fefb57459c1f1236eab9d
binh08042002@gmail.com <binh08042002@gmail.com>
2024-02-13 02:00:36 +0700

    Initial commit after fork

---
+++ src/components/experienceCard/ExperienceCard.js
@@ -0,0 +1,122 @@
+import React, { useState, createRef } from "react";
+import "./ExperienceCard.scss";
+import ColorThief from "colorthief";
+
+export default function ExperienceCard({ cardInfo, isDark }) {
+  const [colorArrays, setColorArrays] = useState([]);
+  const imgRef = createRef();
+
+  function getColorArrays() {
+    const colorThief = new ColorThief();
+    setColorArrays(colorThief.getColor(imgRef.current));
+  }
+
+  function rgb(values) {
+    return typeof values === "undefined"
+      ? null
+      : "rgb(" + values.join(", ") + ")";
+  }
+
+  const ExperienceProjects = ({ projs, isDark }) => {
+    return (
+      <div className="experience-projects">
+        {projs ? projs.map((prj, i) => (
+          <div key={i} className="experience-proj" style={{ display: 'flex', marginBottom: '20px' }}>
+            {prj.videoID && (
+              <div className="video-container">
+                <iframe 
+                  width="560"
+                  height="315"
+                  src={`https://www.youtube.com/embed/${prj.videoID}`}
+                  //allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                  title={prj.title}
+                />
+              </div>
+            )}
+            <div className={isDark ? "description dark-mode-text" : "description"}>
+              <h5>{prj.date} : {prj.title}</h5>
+              <ul>
+                {prj.desc.map((description, index) => (
+                  <li key={index}>+ {description}</li>
+                ))}
+              </ul>
+              {prj.url && prj.url.includes
+                ? <a href={prj.url}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="btn btn-outline-primary"
+                  >
+                    <i className="fab fa-google-play"></i>View on GooglePlay
+                  </a>
+                : null}
+            </div>
+          </div>
+        )) : null}
+      </div>
+    );
+  };
+  
+  return (
+    <div className={isDark ? "experience-card-dark" : "experience-card"}>
+      <div style={{ background: rgb(colorArrays) }} className="experience-banner">
+        <div className="experience-blurred_div"></div>
+        <div className="experience-div-company">
+          <h5 className="experience-text-company">{cardInfo.company}</h5>
+        </div>
+        <img
+          crossOrigin={"anonymous"}
+          ref={imgRef}
+          className="experience-roundedimg"
+          src={cardInfo.companylogo}
+          alt={cardInfo.company}
+          onLoad={() => getColorArrays()}
+        />
+      </div>
+      <div className="experience-text-details">
+        <h5
+          className={
+            isDark
+              ? "experience-text-role dark-mode-text"
+              : "experience-text-role"
+          }
+        >
+          {cardInfo.role}
+        </h5>
+        <h5
+          className={
+            isDark
+              ? "experience-text-date dark-mode-text"
+              : "experience-text-date"
+          }
+        >
+          {cardInfo.date}
+        </h5>
+        <p
+          className={
+            isDark
+              ? "subTitle experience-text-desc dark-mode-text"
+              : "subTitle experience-text-desc"
+          }
+        >
+          {cardInfo.duration}
+        </p>
+        <p
+          className={
+            isDark
+              ? "subTitle experience-text-desc dark-mode-text"
+              : "subTitle experience-text-desc"
+          }
+        >
+          {cardInfo.desc}
+        </p>
+        <h5 className="experience-text-participated-projects">
+        Participated Projects:
+        </h5>
+        <ul>
+          <ExperienceProjects projs={cardInfo.projs} isDark={isDark} />
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ src/portfolio.js
@@ -0,0 +1,372 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
+
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "Phuc-Binh Pham",
+  title:(
+    <>
+      Hi all, I'm Bình,
+      <br />
+      Phạm Phúc Bình
+    </>
+  ),
+  subTitle: emoji(
+    "A passionate Unity Game Developer 🎮 having an experience of developing casual / hypercasual mobile game with Unity Engine."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/binh0804",
+  linkedin: "https://www.linkedin.com/in/knippink/",
+  gmail: "binh08042002@gmail.com",
+  facebook: "https://www.facebook.com/knippinkOwO/",
+  // Instagram, Twitter and Kaggle are also supported in the links!
+  // To customize icons and social links, tweak src/components/SocialMedia
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji(
+      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+    )
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "Unity",
+      fontAwesomeClassname: "fab fa-unity"
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "HCMC University of Technology and Education",
+      logo: require("./assets/images/hcmuteLogo.png"),
+      subHeader: "Bachelor of Infomation Technology",
+      duration: "October 2020 - July 2024",
+      desc: "CGPA: 9.20/10.0 (equivalent to 3.65/4.0). Ranked top 5% in the program.",
+      descBullets: [
+        "Achieved the academic scholarship for 4 out of 7 semesters",
+        "Achieved the title of \"Student of 5 Merits\" in 2020 and 2021"
+      ]
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "90%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "70%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Unity Game Developer",
+      company: "HDGames",
+      companylogo: require("./assets/images/HDGamesLogo.png"),
+      date: "Sep 2022 – Feb 2024",
+      desc: "Developing and maintaining games in the casual genre (hyper-casual and puzzle).",
+      duration: "1 year 5 months",
+      // list of work projects, each project include title, list of description, and url for each
+      projs: [
+        {
+          title: "Apple Worm",
+          date: "Aug 2022 - Sep 2022",
+          desc: [
+            "Designed and implemented 100 levels with intuitive hint, enhancing game complexity, player experience and satisfaction.",
+            "Conducted comprehensive testing to identify and resolve bugs and gameplay issues before release, significantly improving game performance and stability."
+          ],
+          url: "https://play.google.com/store/apps/details?id=com.hdgames.puzzlegame",
+          videoID: "B23FbYgn640"
+        },
+        {
+          title: "Melon Playground",
+          date: "Oct 2022 - Dec 2022",
+          desc: [
+            "Design and develop weapon and item systems, sound, and effects.",
+            "Build games to Android platform, conducted testing, and resolved issues for optimal performance."
+          ],
+          url: "https://play.google.com/store/apps/details?id=com.hdgames.puzzlegame",
+          videoID: "B23FbYgn640"
+        },
+        {
+          title: "Two Player Arcade Games",
+          date: "Aug 2022 - Sep 2022",
+          desc: [
+            "Developed a hyper-casual game with 10 levels.",
+            "Implemented the game's logic, UI, and animations."
+          ],
+          url: "https://play.google.com/store/apps/details?id=com.two.player.arcade.games",
+          videoID: "OBo1oOm8nlQ?si=l_KGCOIWbCwL8ZoR"
+        }
+      ],
+      descBullets: [
+        "02 Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        "02 Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      ]
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "false", // Set true or false to show Contact profile using Github, defaults to true
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/saayaHealthLogo.webp"),
+      projectName: "Saayahealth",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "http://saayahealth.com/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/nextuLogo.webp"),
+      projectName: "Nextu",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "http://nextu.se/"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      subtitle:
+        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+      image: require("./assets/images/codeInLogo.webp"),
+      imageAlt: "Google Code-In Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+        },
+        {
+          name: "Award Letter",
+          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
+        },
+        {
+          name: "Google Code-in Blog",
+          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+        }
+      ]
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle:
+        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/googleAssistantLogo.webp"),
+      imageAlt: "Google Assistant Action Logo",
+      footerLink: [
+        {
+          name: "View Google Assistant Action",
+          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+        }
+      ]
+    },
+
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: require("./assets/images/pwaLogo.webp"),
+      imageAlt: "PWA Logo",
+      footerLink: [
+        {name: "Certification", url: ""},
+        {
+          name: "Final Project",
+          url: "https://pakistan-olx-1.firebaseapp.com/"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
+  blogs: [
+    {
+      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+84 522 942 441",
+  email_address: "binh08042002@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "twitter", //Replace "twitter" with your twitter username without @
+  display: true // Set true to display this section, defaults to false
+};
+
+const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  splashScreen,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+  isHireable
+};
